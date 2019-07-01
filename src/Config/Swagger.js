907229fec3647618ebaf3f031fefd9b14263dafc
Relia <>
2019-07-01 18:18:59 +0700

    change doc's host

--- src/Config/Swagger.js
+++ src/Config/Swagger.js
@@ -2,6 +2,10 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("@Docs");
 
 function setup(app) {
+  if (process.env.NODE_ENV === "production") {
+    swaggerDocument.host = "lapi-test.herokuapp.com";
+  }
+
   app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
