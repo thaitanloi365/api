b72fcd7ddf5167107ae072942ed8b13d176bddf1
Thái Tấn Lợi <macintoshhd@Lois-MacBook-Pro.local>
2019-07-14 10:59:56 +0700

    config server

--- src/Config/Port.js
+++ src/Config/Port.js
@@ -1,5 +1,5 @@
 function setup(app) {
-  app.set("port", process.env.PORT || 3000);
+  app.set("port", process.env.PORT || 5000);
   app.listen(app.get("port"), () => {
     console.log("Express server listening on port %d in %s mode", app.get("port"), app.get("env"));
   });
